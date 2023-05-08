75f4614352261801d767ce20657f680997118912
leenoru <tkdwns828282@gmail.com>
2023-05-08 17:40:36 +0900

    init commit feature of community

---
+++ src/main/java/com/playGG/play/ggprojectboard/Application.java
@@ -0,0 +1,13 @@
+package com.playGG.play.ggprojectboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
