fc87c9cf299d97f55c1954d00de1e0636488b0b0
leenoru <tkdwns828282@gmail.com>
2023-05-09 14:27:35 +0900

    Feat: '추가 테스트코드 작성및SecurityConfig 클래스추가'
    환경설정 및 정상 실행 확인을 위한 단위 테스트코드 작성
    Spring Security 인증 구현을 위한SecurityConfig 클래스 생성

---
+++ src/main/java/com/playGG/play/ggprojectboard/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.playGG.play.ggprojectboard.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@RequiredArgsConstructor
+@EnableWebSecurity  //Spring Security 설정들을 활성화
+@Configuration // Spring 설정 클래스라는 의미를 가짐
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+                .formLogin();
+        return http.build();
+
+    }
+}
---
+++ src/test/java/com/playGG/play/ggprojectboard/web/HelloControllerTest.java
@@ -0,0 +1,32 @@
+package com.playGG.play.ggprojectboard.web;
+
+import com.playGG.play.ggprojectboard.config.SecurityConfig;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(controllers = HelloController.class)
+@Import(SecurityConfig.class)
+class HelloControllerTest {
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    void hello() throws Exception {
+        String hello = "hello";
+
+        mvc.perform(MockMvcRequestBuilders.get("/hello"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string(hello));
+
+    }
+}
