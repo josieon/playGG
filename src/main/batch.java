0e0de294b01b8747fea998caff3df8e932b6dddb
josieon <josieon@naver.com>
2023-05-17 00:31:26 +0900

    Refactor: API_KEY 은닉 처리

--- src/main/batch.java
+++ src/main/batch.java
@@ -9,25 +9,19 @@ import static java.lang.Thread.sleep;
 
 public class batch {
     public static void main(String[] args) throws Exception {
-        long num = 6300098993L;
-//        String matchId = "KR_" + (num);
-
-        for (int i = 1; ;) {
-//            System.out.println(matchId);
-            for(int j = 0; j < 50;i++, j++) {
-                String matchId = "KR_" + (num + i);
-                getMatchDTO.getMatchDto(matchId, "RGAPI-58f34acb-d75d-4d57-ae97-f6f5fb125fb6");
-            }
-            for(int j = 0; j < 50;i++, j++) {
-                String matchId = "KR_" + (num + i);
-                getMatchDTO.getMatchDto(matchId, "RGAPI-82c25212-ef4f-41c6-9725-aafb3f47d070");
-            }
-            for(int j = 0; j < 50;i++, j++) {
-                String matchId = "KR_" + (num + i);
-                getMatchDTO.getMatchDto(matchId, "RGAPI-be0039aa-75df-496a-818a-43e9fbfea52d");
+        long num = 6300766751L;
+        String key = null;
+        for (int i = 1; true;) {
+            BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream("src/API_KEY.txt")));
+            while((key = br.readLine()) != null){
+                for(int j = 0; j < 50; i++, j++) {
+                    String matchId = "KR_" + (num + i);
+                    getMatchDTO.getMatchDto(matchId, key);
+                }
             }
             sleep(60000);
         }
+
 //        Logger logger = Logger.getLogger("mylogger");
 //        String log = "log.log";
 //        FileHandler logFile = null;
